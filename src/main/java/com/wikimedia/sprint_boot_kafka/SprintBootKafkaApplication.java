b41ddaef2dc28d00ce5bc6cc9c22c968cb8fa92e
0APOCALYPSE0 <giriaakash00@gmail.com>
2024-09-21 18:31:12 +0530

    init

---
+++ src/main/java/com/wikimedia/sprint_boot_kafka/SprintBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.wikimedia.sprint_boot_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootKafkaApplication.class, args);
+	}
+
+}
